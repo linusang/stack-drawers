1c43c154eaf280bedc5d7f92fec49fc422f53605
Linus Ang <linusang@yahoo.com>
2023-06-01 12:31:07 +0800

    use createWebHashHistory instead

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import "./App.css";
 
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import App from "./App.vue";
 import stackDrawersPlugin from "./components/stackDrawersPlugin";
 import { routes } from "./routes";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
